ad826940e246e7594a363950fe8a5e7c08ef3847
Soheil Norouzi <soheil.nrz@gmail.com>
2016-06-29 15:21:55 -0700

    Minor fixes

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,12 +1,12 @@
 from media import Movie
 
 movies = []
-movies.append(Movie("Finding Dory", \
-  "http://t1.gstatic.com/images?q=tbn:ANd9GcTPxyoxzLf33_chM3uqooaT3tiyEBbQmqJb0Ndbvpt6qfQ4ybIk", \
+movies.append(Movie("Finding Dory", # noqa
+  "http://t1.gstatic.com/images?q=tbn:ANd9GcTPxyoxzLf33_chM3uqooaT3tiyEBbQmqJb0Ndbvpt6qfQ4ybIk", # noqa
   "https://www.youtube.com/watch?v=cxskm6dDfIY")) # noqa
-movies.append(Movie("Warcraft", \
-  "http://t3.gstatic.com/images?q=tbn:ANd9GcTCzditN2MtHjUM51mMbNspnJ_NHPWYz3J8zQMoXPRtAeBTpTrf", \
+movies.append(Movie("Warcraft", # noqa
+  "http://t3.gstatic.com/images?q=tbn:ANd9GcTCzditN2MtHjUM51mMbNspnJ_NHPWYz3J8zQMoXPRtAeBTpTrf", # noqa
   "https://www.youtube.com/watch?v=7p43xwId1eE")) # noqa
-movies.append(Movie("The Jungle Book", \
-  "http://t1.gstatic.com/images?q=tbn:ANd9GcQgNaB5wtt0G7_mTFVygkFtmjWut_Z0QSz2GzDsHeiZDHno4fjh", \
+movies.append(Movie("The Jungle Book", # noqa
+  "http://t1.gstatic.com/images?q=tbn:ANd9GcQgNaB5wtt0G7_mTFVygkFtmjWut_Z0QSz2GzDsHeiZDHno4fjh", # noqa
   "https://www.youtube.com/watch?v=DlRPZjxtetI")) # noqa
